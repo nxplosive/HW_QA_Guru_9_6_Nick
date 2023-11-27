15554fc2735209b49d24f8e1127ec32797632690
Nikita <xicat5000@yandex.ru>
2023-11-27 16:03:19 +0300

    Updated

--- test_homework_9_6.py
+++ test_homework_9_6.py
@@ -1,3 +1,4 @@
+import datetime
 from datetime import time
 
 
@@ -23,17 +24,17 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
-    current_time = time(hour=16)
+    current_time = time(hour=23)
     dark_theme_enabled_by_user = True
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    if dark_theme_enabled_by_user is True:
+    if dark_theme_enabled_by_user:
         is_dark_theme = True
-    elif dark_theme_enabled_by_user is False:
-        is_dark_theme = False
     elif dark_theme_enabled_by_user is None:
-        if time(hour=6) > current_time > time(hour=22):
+        if datetime.time(6) < current_time < datetime.time(22):
+            is_dark_theme = False
+        else:
             is_dark_theme = True
     else:
         is_dark_theme = False
